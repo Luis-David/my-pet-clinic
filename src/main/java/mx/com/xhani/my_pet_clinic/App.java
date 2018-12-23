0822df9c3d5582f578dde18f1233f2f403769804
Luis David <luisdaviduacm@gmail.com>
2018-12-22 20:59:01 -0600

    primer avances

---
+++ src/main/java/mx/com/xhani/my_pet_clinic/App.java
@@ -0,0 +1,13 @@
+package mx.com.xhani.my_pet_clinic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
